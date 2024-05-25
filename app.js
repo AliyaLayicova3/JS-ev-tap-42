4ecb66b3b06ae6b6327cbfb7c1077ff3d1a0e286
Layicovaaliya <162481015+Layicovaaliya@users.noreply.github.com>
2024-05-25 16:25:02 +0400

    Qarabağ

---
+++ app.js
@@ -0,0 +1,42 @@
+let qarabağ = prompt("Rayon ve ya Şeher adı yaz")
+switch (qarabağ) {
+    case "Cebrayıl":
+        console.log("Cebrayıl rayonu 04.10.2020-ci il tarixinde işğaldan azad olunub")
+        break;
+    case "Füzuli":
+        console.log("Füzuli rayonu 17.10.2020-ci il tarixinde işğaldan azad olunub")
+        break;
+    case "Zengilan":
+        console.log("Zengilan rayonu 20.10.2020-ci il tarixinde işğaldan azad olunub")
+        break;
+    case "Qubadlı":
+        console.log("Qubadlı rayonu 25.10.2020-ci il tarixinde işğaldan azad olunub")
+        break;
+    case "Şuşa":
+        console.log("Şuşa şeheri 08.11.2020-ci il tarixinde işğaldan azad olunub")
+        break;
+    case "Xankendi":
+        console.log("Xankendi şeheri 10.11.2020-ci il tarixinde işğaldan azad olunub")
+        break;
+    case "Xocalı":
+        console.log("Xocalı rayonu 10.11.2020-ci il tarixinde işğaldan azad olunub")
+        break;
+    case "Xocavend":
+        console.log("Xocavend rayonu 10.11.2020-ci il tarixinde işğaldan azad olunub")
+        break;
+    case "Ağdere":
+        console.log("Ağdere rayonu 10.11.2020-ci il tarixinde işğaldan azad olunub")
+        break;
+    case "Ağdam":
+        console.log("Ağdam rayonu 20.11.2020-ci il tarixinde işğaldan azad olunub")
+        break;
+    case "Kelbecer":
+        console.log("Kelbecer rayonu 25.11.2020-ci il tarixinde işğaldan azad olunub")
+        break;
+    case "Laçın":
+        console.log("Laçın rayonu 01.12.2020-ci il tarixinde işğaldan azad olunub")
+        break;
+    default:
+        document.write("Bu adla Rayon ve ya Şeher yoxdu")
+        break;
+    }
